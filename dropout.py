baf83ab57c66ec040deab2d0242f3ef8ea4b5bf9
JoshLoecker <joshloecker@icloud.com>
2020-10-13 18:24:50 -0500

    Updated master with dropout branch

--- dropout.py
+++ dropout.py
@@ -1,93 +1,93 @@
-# from: https://towardsdatascience.com/understanding-and-implementing-dropout-in-tensorflow-and-keras-a8a3a02c1bfa
+# initial article on dropout networks: https://towardsdatascience.com/understanding-and-implementing-dropout-in-tensorflow-and-keras-a8a3a02c1bfa
+# current code from: https://github.com/musikalkemist/DeepLearningForAudioWithPython/blob/master/14-%20Solving%20overfitting%20in%20neural%20networks/code/solving_overfitting.py
+# video on code at: https://www.youtube.com/watch?v=Gf5DO6br0ts&list=PL-wATfeyAMNrtbkCNsLcpoAyBBRJZVlnf&index=14
+# This seemed to be more simple than the class implementation; easier to see what network contains
 import tensorflow as tf
 from tensorflow import keras
 import sys
+from pprint import pprint
+import matplotlib.pyplot as plt
+import os
 
 
-class RegularNetwork(keras.Model):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.input_layer = keras.layers.Flatten(input_shape=(28, 28))
-        self.hidden1 = keras.layers.Dense(200, activation='relu')
-        self.hidden2 = keras.layers.Dense(100, activation='relu')
-        self.hidden3 = keras.layers.Dense(60, activation='relu')
-        self.output_layer = keras.layers.Dense(10, activation='softmax')
+def DropoutNetwork(input_data):
+    model = keras.Sequential([
+        # input layer; flatten will make a 2D array [[1, 2], [3, 4]] into a 1D array [1, 2, 3, 4]
+        keras.layers.Flatten(input_shape=(input_data.shape[1], input_data.shape[2])),
 
-    def call(self, input, training=None):
-        input_layer = self.input_layer(input)
-        hidden1 = self.hidden1(input_layer)
-        hidden2 = self.hidden2(hidden1)
-        hidden3 = self.hidden3(hidden2)
-        output_layer = self.output_layer(hidden3)
-        return output_layer
+        # first dense layer
+        keras.layers.Dense(200, activation='relu'),
+        keras.layers.Dropout(0.3),
 
+        # second dense layer
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dropout(0.3),
 
-class DropoutNetwork(keras.Model):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.input_layer = keras.layers.Flatten(input_shape=(28, 28))
-        self.hidden1 = keras.layers.Dense(200, activation='relu')
-        self.hidden2 = keras.layers.Dense(100, activation='relu')
-        self.hidden3 = keras.layers.Dense(60, activation='relu')
-        self.output_layer = keras.layers.Dense(10, activation='softmax')
-        self.dropout_layer = keras.layers.Dropout(rate=0.2)
+        # third dense layer
+        keras.layers.Dense(60, activation='relu'),
+        keras.layers.Dropout(0.3),
 
-    def call(self, input, training=None):
-        input_layer = self.input_layer(input)
-        input_layer = self.dropout_layer(input_layer)
-        hidden1 = self.hidden1(input_layer)
-        hidden1 = self.dropout_layer(hidden1, training=training)
-        hidden2 = self.hidden2(hidden1)
-        hidden2 = self.dropout_layer(hidden2, training=training)
-        hidden3 = self.hidden3(hidden2)
-        hidden3 = self.dropout_layer(hidden3, training=training)
-        output_layer = self.output_layer(hidden3)
-        return output_layer
+        # output layer
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    # compile model/graph
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimizer,
+                  loss="sparse_categorical_crossentropy",
+                  metrics=['accuracy'])
+
+    return model
 
 
-def evaluate(input_model, train_images, train_labels, validation_images, validation_labels, epoch):
-    sgd = keras.optimizers.SGD(lr=0.01)
-    input_model.compile(loss="sparse_categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
-    input_model.fit(train_images, train_labels, epochs=epoch, validation_data=(validation_images, validation_labels))
+def plot_history(history):
 
-    loss, accuracy = input_model.evaluate(test_images, test_labels)
-    return loss, accuracy
+    # create a new figure with two subplots
+    fig, axs = plt.subplots(2)
 
+    # history is a dictionary
+    # history.history has four values: accuracy, val_accuracy, loss, val_loss
+    # create new plot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
 
-def dropout(data: list, epoch):
-    model = DropoutNetwork()
-    return evaluate(model, data[0], data[1], data[2], data[3], epoch)
+    # create error subplot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
 
-
-def regular(data: list, epoch):
-    model = RegularNetwork()
-    return evaluate(model, data[0], data[1], data[2], data[3], epoch)
+    os.makedirs("./results", exist_ok=True)
+    plt.savefig("./results/epoch_results.png")
 
 
 if __name__ == '__main__':
-    # set up a default epoch to be used
-    args = sys.argv
-    try:
-        epoch_value = int(args[1])
-    except IndexError:
-        epoch_value = 60  # default value of 60
+    epoch_value = 1
 
+    # load data set
     (train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
+
+    # convert data set to be between 0 - 1
     train_images = train_images / 255.0
     test_images = test_images / 255.0
+
+    # set aside validation images
     validation_images = train_images[:5000]
     validation_labels = train_labels[:5000]
 
-    data = [train_images, train_labels, validation_images, validation_labels]
+    # create a dropout network
+    model = DropoutNetwork(train_images)
 
-    dropout_loss, dropout_accuracy = dropout(data, epoch_value)
-    reg_loss, reg_accuracy = regular(data, epoch_value)
+    # fit our model to data
+    history = model.fit(train_images, train_labels, epochs=100, validation_data=(validation_images, validation_labels), batch_size=32)
 
-    print("\n\n")
-    print("Dropout")
-    print(f"Loss: {dropout_loss}")
-    print(f"Accuracy: {dropout_accuracy}")
+    # evaluate model with testing data
+    loss, accuracy = model.evaluate(test_images, test_labels)
 
-    print("\nRegular")
-    print(f"Loss: {reg_loss}")
-    print(f"Accuracy: {reg_accuracy}")
+    # view a history of data
+    plot_history(history)
